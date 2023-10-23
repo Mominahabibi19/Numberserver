b233206797658decf580a85ce324f0901e88c38f
Mominahabibi19 <146862600+Mominahabibi19@users.noreply.github.com>
2023-10-23 11:06:49 -0700

    Update TestNumberServer.java

--- TestNumberServer.java
+++ TestNumberServer.java
@@ -4,4 +4,17 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class TestNumberServer{
+  @Test
+  public void testIncrement () throws URISyntaxException {
+ Handler h = new Handler();
+    URI increment = new URI("http://localhost/increment");
+    URI rootPath = new URI("http://localhost/");
+    assertEquals("Number incremented!", h.handleRequest(increment));
+    assertEquals("Number:1 ", h.handleRequest(rootPath));
+    
+      assertEquals(, h.handleRequest(increment));
+    assertEquals(, h.handleRequest(rootPath));
+    
+  } 
+}
   
